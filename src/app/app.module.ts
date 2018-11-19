b273137aec636657cf0147e79adb7bdbedd0185d
jannesmelis <jannes.melis@live.be>
2018-11-19 10:44:43 +0100

    kdfjskd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgBootstrapModule} from './sharedModules/ng-bootstrap.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    
+    NgBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
