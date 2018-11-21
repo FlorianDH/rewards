4152b0ff7392dfb90411bb3d6cac3d22480ba127
FlorianDHaene <dhaene_florian@hotmail.com>
2018-11-21 12:54:22 +0100

    challenge rework

--- src/app/challenge/challenge.component.ts
+++ src/app/challenge/challenge.component.ts
@@ -1,9 +1,8 @@
 
 import { Component, OnInit, ReflectiveInjector, Input } from '@angular/core';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbActiveModal, NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 import { DataService } from '../services/data.service';
 import { ChallengeService } from '../services/challenge.service';
-
 import { Challenge } from '../interfaces/challenge';
 import { Request } from '../interfaces/request';
 import { formatDate } from '@angular/common';
@@ -14,42 +13,44 @@ import { formatDate } from '@angular/common';
   templateUrl: './challenge.component.html'
 })
 export class ChallengeComponent implements OnInit {
+  closeResult: string;
 
-
-
-  challengesList : Challenge[] = [];
-  @Input() request : Request;
+  challengesList: Challenge[] = [];
+  @Input() request: Request;
   today = new Date ();
-  jstoday ='';
-  
-  constructor(public challengeService : ChallengeService) {  }
+  jstoday = '';
 
-  
-  ngOnInit() {   
-    console.log("hallo test");
+  constructor(public challengeService: ChallengeService, private modalService: NgbModal) {}
+
+  ngOnInit() {
     this.challengesList = this.challengeService.getChallenges();
-    
-    
-
-    this.request = {
-      points : "",
-    title : "",
-    _id : "",
-    motivation : "",
-    challenge_id : "",
-    date : "",
-    isAccepted : false,
-    };
   }
 
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
 
-  challengeExecuted(i){
-    
-    
+  private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return  `with: ${reason}`;
+      }
+    }
+
+  challengeExecuted(i) {
+
+
     this.jstoday = formatDate(this.today, 'dd-MM-yyyy hh:mm:ss a', 'en-US', '+00:00');
 
 
-    
+
 
     console.log("HIER MOET EEN TITEL KOMEN :" + this.challengesList[i].title);
     console.log("HIER MOET EEN TITEL KOMEN :" + this.challengesList[i].points);
@@ -62,18 +63,18 @@ export class ChallengeComponent implements OnInit {
     this.request = {
       points : this.challengesList[i].points,
       title : this.challengesList[i].title,
-      _id : "",
+      _id : '',
       motivation : motivation,
       challenge_id : this.challengesList[i]._id,
       date : this.jstoday,
       isAccepted : false,
-      }
+      };
 
-      //this.challengeService.addChallangeRequest(this.request).subscribe(test => this)
+      // this.challengeService.addChallangeRequest(this.request).subscribe(test => this)
 
     }
 
 
-   
+
   }
 
