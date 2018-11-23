bfded1132d31d8bba165f06b76d6974d8d758466
FlorianDHaene <dhaene_florian@hotmail.com>
2018-11-23 16:00:02 +0100

    reward modal beginsel

--- src/app/reward/reward-item/reward-item.component.ts
+++ src/app/reward/reward-item/reward-item.component.ts
@@ -1,4 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import {RewardService} from '../../services/reward.service';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+// import {Claim}
+import {formatDate} from '@angular/common';
 
 @Component({
   selector: 'app-reward-item',
@@ -8,9 +12,17 @@ import { Component, Input, OnInit } from '@angular/core';
 export class RewardItemComponent implements OnInit {
 
   @Input() rewardsList: any;
-  Constructor() { }
+
+  user = JSON.parse(localStorage.getItem('user'));
+
+  // Constructor(public rewardService: RewardService, private modalService: NgbModal) { }
+  // today = new Date();
+  // jstoday = '';
 
   ngOnInit() {
   }
 
+  open(content, i) {
+    // this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {}, (reason) => {});
+  }
 }
