8616107f2aceaccecfac032f583ebe26a0f7ee4b
jens-vanhoof <jens.vanhoof@immalle.be>
2018-11-24 20:22:09 +0100

    output databinding

--- src/app/reward/reward.component.ts
+++ src/app/reward/reward.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RewardService} from '../services/reward.service';
 import { Reward} from '../interfaces/reward';
+import { AnonymousSubject } from 'rxjs/internal/Subject';
+import { User } from '../interfaces/user';
 
 @Component({
   selector: 'app-reward',
@@ -9,11 +11,12 @@ import { Reward} from '../interfaces/reward';
 export class RewardComponent implements OnInit {
   user = JSON.parse(localStorage.getItem('user'));
   rewardsList: Reward[] = [];
-  user = JSON.parse(localStorage.getItem('user'));
-
   constructor(private rewardService: RewardService) {}
-
+  punten = this.user.currentPoints;
   ngOnInit() {
     this.rewardsList = this.rewardService.getRewards();
+  }
+  receiveValue($event){
+    this.punten = $event;
   }
 }
