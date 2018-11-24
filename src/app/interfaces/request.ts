f06a101d1e513b9e558431cd997b4b8756e58363
jannesmelis <jannes.melis@live.be>
2018-11-24 14:44:44 +0100

    voor token

--- src/app/interfaces/request.ts
+++ src/app/interfaces/request.ts
@@ -1,16 +1,8 @@
 export interface Request {
-<<<<<<< HEAD
     motivation : String;
     _id : String,
     challenge_id : any;
     date : String;
     isAccepted : Boolean;
     user_id : any;
-=======
-    motivation: String;
-    challenge_id: any;
-    date: String;
-    isAccepted: Boolean;
-    user_id: any;
->>>>>>> fe693d9a5e4326492e0a1e4278540383d25d92e1
 }
